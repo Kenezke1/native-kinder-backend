368e0073297b0a8ed92599408ed72e73bf252226
Kenezke1 <herczeg.kenya2@gmail.com>
2018-09-21 15:20:48 +0200

    Implement security config

---
+++ src/main/java/com/codecool/kinder/SecurityConfig.java
@@ -0,0 +1,75 @@
+package com.codecool.kinder;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@EnableWebMvc
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/send").permitAll();
+
+        http.authorizeRequests()
+                .antMatchers("/**", "/index").permitAll();
+        http.authorizeRequests()
+                .antMatchers("/admin**").access("hasRole('ROLE_ADMIN')")
+                .antMatchers(HttpMethod.GET, "/**").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+                .antMatchers(HttpMethod.POST, "/**").access("hasRole('ADMIN')")
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .logout().permitAll()
+                .and().httpBasic();
+        http.csrf().disable();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    UserDetailsManager userDetailsManager(
+            @Autowired DataSource dataSource,
+            @Autowired JdbcTemplate jdbcTemplate,
+            @Autowired AuthenticationManager authenticationManager) {
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager();
+        userDetailsManager.setJdbcTemplate(jdbcTemplate);
+        userDetailsManager.setDataSource(dataSource);
+        // Using AuthenticationManager re-authenticates the session on password change.
+        userDetailsManager.setAuthenticationManager(authenticationManager);
+        return userDetailsManager;
+    }
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
