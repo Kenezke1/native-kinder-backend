f4f3690eb4cfdaca5a2ae341aeb2ec3ad296c633
Kenezke1 <herczeg.kenya2@gmail.com>
2018-10-04 11:50:35 +0200

    Bugfix

--- src/test/java/com/codecool/kinder/KinderApplicationTests.java
+++ src/test/java/com/codecool/kinder/KinderApplicationTests.java
@@ -3,9 +3,11 @@ package com.codecool.kinder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class KinderApplicationTests {
 
